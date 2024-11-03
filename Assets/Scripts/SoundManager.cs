92ef158e6005f8c0bcf5d115beadd41833b9d18f
ingolfTHEcapitan <vitaly.litov4encko2017@gmail.com>
2024-11-03 20:18:16 +1000

    refactor(sound): убран синглтон в классе SoundManager Он был не нужен так как воспроизведение звука реализууется через подписки на события в классе SoundManager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,29 +2,20 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-	private static SoundManager s_instanñe = null;
+	[Header("Sources")]
 	[SerializeField] private AudioSource _efxSource1;
 	[SerializeField] private AudioSource _efxSource2;
-
+	
+	[Header("Clips")]
 	[SerializeField] private AudioClip _gameOverSound;
 	[SerializeField] private AudioClip _eatFoodSound;
 	[SerializeField] private AudioClip _moveSound;
 
 	void Awake()
 	{
-		// Ðåàëèçàöèÿ ïàòòåðíà ñèíãëòîí
-		if (s_instanñe == null)
-			s_instanñe = this;
-		else if (s_instanñe != this) 
-			Destroy(gameObject);
-			
-		DontDestroyOnLoad(gameObject);
-		
-		// Ïîäïèñêè íà ñîáûòèå
-		EventManager.SnakeDiedEvent.AddListener(() => PlaySound(_efxSource1, _gameOverSound));
-		EventManager.FoodÅatenEvent.AddListener(() => PlaySound(_efxSource1, _eatFoodSound));
-		EventManager.SnakeMovedEvent.AddListener(() => PlaySound(_efxSource2, _moveSound));
-		
+		GameEvents.SnakeDied.AddListener(() => PlaySound(_efxSource1, _gameOverSound));
+		GameEvents.FoodÅaten.AddListener(() => PlaySound(_efxSource1, _eatFoodSound));
+		GameEvents.SnakeMoved.AddListener(() => PlaySound(_efxSource2, _moveSound));
 	}
 
 	public void PlaySound(AudioSource source, AudioClip clip)
